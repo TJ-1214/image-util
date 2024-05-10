700c335ad0f91d2aeab451e01ece854a500923ff
tant <tant@core-app.com>
2024-05-10 18:16:28 +0800

    Added README

---
+++ README.md
@@ -0,0 +1,31 @@
+Certainly! Here's a README.md description for the given dependency:
+
+---
+
+# Image Util Library - Version 1.0.0
+
+This is a lightweight Java library for handling image input and output operations efficiently. It offers simple yet powerful functionalities to work with various image formats.
+
+## Installation
+
+To use this library in your Java project, add the following dependency to your project's `pom.xml` file:
+
+```xml
+<dependency>
+    <groupId>io.tj</groupId>
+    <artifactId>io.image</artifactId>
+    <version>1.0.0</version>
+</dependency>
+```
+
+Make sure you have Maven configured in your project to resolve the dependencies automatically.
+
+
+## Contribution
+
+Contributions to this project are welcome! If you encounter any issues or have suggestions for improvements, feel free to open an issue or submit a pull request on [GitHub](https://github.com/example/repository).
+
+
+---
+
+Feel free to adjust the sections or add more details based on your specific needs or preferences!
--- src/main/java/io/image/util/ImageUtil.java
+++ src/main/java/io/image/util/ImageUtil.java
@@ -2,6 +2,28 @@ package io.image.util;
 
 public class ImageUtil {
 
+	
+	/*
+	 * 1. **JPEG (Joint Photographic Experts Group)**: - Magic Bytes: FF D8 FF -
+	 * Description: JPEG files typically start with the byte sequence FF D8 FF.
+	 * 
+	 * 2. **PNG (Portable Network Graphics)**: - Magic Bytes: 89 50 4E 47 0D 0A 1A
+	 * 0A - Description: PNG files begin with the byte sequence 89 50 4E 47 0D 0A 1A
+	 * 0A.
+	 * 
+	 * 3. **GIF (Graphics Interchange Format)**: - Magic Bytes: 47 49 46 38 37 61
+	 * (GIF87a) or 47 49 46 38 39 61 (GIF89a) - Description: GIF files start with
+	 * the byte sequence 47 49 46 38 37 61 (for GIF87a) or 47 49 46 38 39 61 (for
+	 * GIF89a).
+	 * 
+	 * 4. **BMP (Bitmap)**: - Magic Bytes: 42 4D - Description: BMP files begin with
+	 * the byte sequence 42 4D.
+	 * 
+	 * 5. **TIFF (Tagged Image File Format)**: - Magic Bytes: 49 49 2A 00
+	 * (little-endian) or 4D 4D 00 2A (big-endian) - Description: TIFF files start
+	 * with either 49 49 2A 00 (little-endian) or 4D 4D 00 2A (big-endian).
+	 */
+	
 /**
  * 
  * @param data byte arrays
